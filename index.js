4f508d225c3c9f1fa6f195bf1353b393bf7abb10
Mahmod Hassan <mahmodhasan7788@gmail.com>
2022-10-25 21:32:20 +0600

    fake courses data create

--- index.js
+++ index.js
@@ -6,9 +6,11 @@ const port = process.env.PORT || 5000;
 
 app.use(cors());
 
+const courses = require('./data/courses.json');
 app.get('/', (req, res) => {
     res.send('javascript learning site server')
 })
+
 app.listen(port, () => {
     console.log('this site running on port', port);
 })
